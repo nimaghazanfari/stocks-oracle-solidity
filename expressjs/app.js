7a998243d6f1f1d228d06110bb54ed2b366706b9
nimaghazanfari <ghazanfari_nima@yahoo.com>
2021-06-21 04:03:03 +0430

    expressjs works good!

--- expressjs/app.js
+++ expressjs/app.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const express = require('express');
 const axios = require('axios');
+const fs = require('fs');
+const Web3 = require('web3');
 const app = express();
 const httpServer = require('http').createServer(app);
 
@@ -20,11 +22,28 @@ httpServer.listen(port, () => {
     console.log('Listening to port:', port);
 });
 
-//express endpoints
-
 const pathToAbi = path.resolve('../blockchain/build/contracts/Stocks.json');
+//default ganache-cli setup
+const web3 = new Web3('http://localhost:8545');
 
-//this will get the api for Stocks contract
+const getContract = () => {
+    const artifact = fs.readFileSync(pathToAbi, 'utf-8');
+
+    const json = JSON.parse(artifact);
+    const networks = json.networks;
+    const addresss = networks[Object.keys(networks)[0]].address;
+
+    const contract = new web3.eth.Contract(json.abi, addresss);
+
+    return contract;
+}
+
+const getOwner = async () => {
+    const [owner] = await web3.eth.getAccounts();
+    return owner;
+}
+
+//express endpoints
 app.get('/get-stocks-abi', (req, res) => {
     res.header("Content-Type", 'application/json');
     res.sendFile(pathToAbi);
@@ -33,8 +52,37 @@ app.get('/get-stocks-abi', (req, res) => {
 app.get('/query/:what', async (req, res) => {
     res.header("Content-Type", 'application/json');
     const symbol = req.params.what;
+    if (symbol.length > 4) {
+        res.send('Error!');
+        return;
+    }
+
     const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=KEY`;
 
     const result = await axios.get(url);
-    res.send('Query Done!');
+
+    if (result.data && result.data["Global Quote"]) {
+
+        const price = parseInt(result.data["Global Quote"]["05. price"]);
+        const volume = parseInt(result.data["Global Quote"]["06. volume"]);
+
+        if (isNaN(price) || isNaN(volume)) {
+            res.send('Invalid Request!');
+            return;
+        }
+
+        const owner = await getOwner();
+        const contract = getContract();
+
+        try {
+            //set price on each request into blockchain
+            await contract.methods.setStock(web3.utils.fromAscii(symbol), price, volume).send({ from: owner });
+
+            res.send('Query Done!');
+        } catch (error) {
+            console.log(error);
+            res.send('Error Happened!');
+        }
+    }
+    else res.send('Empty Result!');
 });
